27d9588e448103f31f6279619ec675b5a1522673
Mahin Abdullah <mahinabdullah961@gmail.com>
2026-02-18 17:57:55 +0600

    Changed folder structure

---
+++ 00_Variables and Strings/01_Workshop/01_Build a Greeting Bot/script.js
@@ -0,0 +1,61 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+
+let bot;
+
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+let botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+let botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+bot = "professorBot";
+
+let nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+let newNicknameGreeting =
+  "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+let favoriteSubject = "Computer Science";
+
+let favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!");
+
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "sweet";
+noun = "human";
+verb = "build";
+place = "earth";
+adjective2 = "cool";
+noun2 = "pizza";
+
+let firstStory;
+
+firststory =
+  "Once upon a time, there was a "+adjective+" "+ noun+ " who loved to eat "+ noun2+ ". The " + noun + " lived in a "+place+ " and had "+ adjective2 +" nostrils that blew fire when it was "+ verb+ ".";
+
+console.log("First story: "+firststory);
+
+let secondStory;
+
+secondStory = "Once upon a time, there was a "+adjective2+" "+ noun2+ " who loved to eat "+ noun+ ". The " + noun2 + " lived in a "+place+ " and had "+ adjective +" nostrils that blew fire when it was "+ verb+ ".";
+
+console.log("Second story: "+secondStory);
